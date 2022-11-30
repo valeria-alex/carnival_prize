0d647807906255bc9e6b75b46dce69d83fbe397b
valeria-alex <47971956+valeria-alex@users.noreply.github.com>
2022-11-30 23:00:01 +0100

    first commit

---
+++ imps.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import numpy
+import random
+
+def luck(l=None):
+        chances = []
+        if l == None:
+                print ('please provide a list')
+        else:
+                #each element is assigned a chance, but the sum of chances across element must be 100
+                max = 100
+                for i in range(len(l)):
+                        
+                        chance = random.randint(0,max + 1)
+                        
+                        #ensure all chances add up to 100
+                        if i == len(l) - 1:
+                                chance = max
+                        else: 
+                                max -= chance
+                        chances.append(chance)
+                        print (i, l[i], chance)
+                #the highest chance wins
+        return chances
+
+
+print ("check python")
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+thisdict = {
+  "games": ["Elden Ring", "League of Legends", "Minecraft", "Stray"],
+  "video": ["Vlog", "Storytelling", "Cooking"],
+  "code":  ["Python", "C++", "scss", "Java", "Typescript"],
+  "exercise": ["bike", "gym", "dance", "walk", "bouldering"]
+  #"relax": ["movie", "hangout"]
+}
+labels = list(thisdict.keys())
+sizes = luck(labels)#[15, 30, 45, 10]
+
+explode = (0, 0, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+ 
+value = input('Let us start')
+if value != None:
+    plt.cla()
+    explode = (0, 0.1, 0, 0)
+    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+plt.show()
